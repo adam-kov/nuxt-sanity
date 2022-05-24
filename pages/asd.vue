fef5ec6f84b2381d657db1661387495caccf80a5
Ádám Kovács <vartefol@gmail.com>
2022-05-24 23:11:25 +0200

    Init

---
+++ pages/asd.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script lang="js">
+
+export default {
+  name: 'IndexPage',
+  inject: ['$chakraColorMode', '$toggleColorMode'],
+  data () {
+    return {
+      showModal: false,
+      mainStyles: {
+        dark: {
+          bg: 'gray.700',
+          color: 'whiteAlpha.900'
+        },
+        light: {
+          bg: 'white',
+          color: 'gray.900'
+        }
+      }
+    }
+  },
+  computed: {
+    colorMode () {
+      return this.$chakraColorMode()
+    },
+    theme () {
+      return this.$chakraTheme()
+    },
+    toggleColorMode () {
+      return this.$toggleColorMode
+    }
+  },
+  methods: {
+    showToast () {
+      this.$toast({
+        title: 'Account created.',
+        description: "We've created your account for you.",
+        status: 'success',
+        duration: 10000,
+        isClosable: true
+      })
+    }
+  }
+}
+</script>
